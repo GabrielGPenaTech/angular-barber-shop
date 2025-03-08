99f599cc128ff458dc4c8e7745cd575b21cbb45c
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-08 19:10:40 -0300

    feat: enhance schedule calendar component with lifecycle hooks and delete confirmation dialog

--- src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
+++ src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,13 +1,24 @@
-import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
-import { FormControl, FormsModule} from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  Output,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+  AfterViewInit,
+  type OnDestroy
+} from '@angular/core';
+import { FormControl, FormsModule, type NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import {  MatCardModule } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import {  MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
-import {  MatTimepickerModule } from '@angular/material/timepicker';
+import { MatTimepickerModule } from '@angular/material/timepicker';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SERVICES_TOKEN } from '../../../services/service.token';
 import { DialogManagerService } from '../../../services/dialog-manager.service';
@@ -20,6 +31,8 @@ import {
 } from '../../schedule.models';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
+import { YesNoDialogComponent } from '../../../commons/components/yes-no-dialog/yes-no-dialog.component';
+import type { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-schedule-calendar',
@@ -41,10 +54,12 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './schedule-calendar.component.html',
   styleUrl: './schedule-calendar.component.scss',
   providers: [
-    {provide: SERVICES_TOKEN.DIALOG, useClass: DialogManagerService}
+    { provide: SERVICES_TOKEN.DIALOG, useClass: DialogManagerService }
   ]
 })
-export class ScheduleCalendarComponent {
+export class ScheduleCalendarComponent implements AfterViewInit, OnChanges, OnDestroy {
+
+  private subscription?: Subscription
 
   private _selected: Date = new Date()
 
@@ -65,8 +80,95 @@ export class ScheduleCalendarComponent {
   @Output() onConfirmDelete = new EventEmitter<ClientScheduleAppointmentModel>()
   @Output() onScheduleClient = new EventEmitter<SaveScheduleModel>()
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
   constructor(
     @Inject(SERVICES_TOKEN.DIALOG) private readonly dialogManagerService: DialogManagerService
-  ) {}
+  ) { }
 
+  getSelected(): Date {
+    return this._selected
+  }
+
+  setSelected(selected: Date) {
+    if(this._selected.getTime() !== selected.getTime()) {
+      this.onDateChange.emit(selected)
+      this.buildTable()
+      this._selected = selected
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if(this.dataSource && this.paginator) {
+      this.dataSource.paginator = this.paginator
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['monthSchedule'] && this.monthSchedule) {
+      this.buildTable()
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  }
+
+  onTimeChange(time: Date) {
+    const endAt = new Date(time)
+    endAt.setHours(time.getHours() + 1)
+    this.newSchedule.endAt = endAt
+  }
+
+  onSubmit(form: NgForm) {
+    const startAt = new Date(this._selected)
+    const endAt = new Date(this._selected)
+    startAt.setHours(this.newSchedule.startAt!.getHours(), this.newSchedule.startAt!.getMinutes())
+    endAt.setHours(this.newSchedule.endAt!.getHours(), this.newSchedule.endAt!.getMinutes())
+
+    const saved: ClientScheduleAppointmentModel = {
+      id: -1,
+      day: this._selected.getDate(),
+      startAt,
+      endAt,
+      clientId: this.newSchedule.clientId!,
+      clientName: this.clients.find(client => client.id === this.newSchedule.clientId)!.name
+    }
+
+    this.onScheduleClient.emit(saved)
+    this.buildTable()
+    form.resetForm()
+    this.newSchedule = {}
+  }
+
+  requestDelete(schedule: ClientScheduleAppointmentModel) {
+    this.subscription = this.dialogManagerService.showYesNoDialog(YesNoDialogComponent,
+      {title: 'Exclusão do Agendamento', content: `Confirma a exclusão do agendamento?`}
+    )
+    .subscribe(result => {
+      if(result) {
+        this.onConfirmDelete.emit(schedule)
+        const updatedList = this.dataSource.data.filter(dataRowTable => dataRowTable.id != schedule.id)
+        this.dataSource = new MatTableDataSource<ClientScheduleAppointmentModel>(updatedList)
+        if (this.paginator) {
+          this.dataSource.paginator = this.paginator
+        }
+      }
+    })
+  }
+
+  private buildTable() {
+    const appointments = this.monthSchedule.scheduleAppointments.filter(a =>
+      this.monthSchedule.year === this._selected.getFullYear() &&
+      this.monthSchedule.month === this._selected.getMonth() &&
+      a.day === this._selected.getDate()
+    )
+
+    this.dataSource = new MatTableDataSource<ClientScheduleAppointmentModel>(appointments)
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator
+    }
+  }
 }
