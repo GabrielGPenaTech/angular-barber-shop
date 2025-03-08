b510bd5c3cc32cebbb106a54f7e94aef410e0c7c
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-08 02:40:12 -0300

    feat: implement schedule service with CRUD operations and models

--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -2,11 +2,12 @@ import { InjectionToken } from "@angular/core";
 import { IClientService } from "./api-client/clients/iclients.service";
 import { ISankBarManagerService } from "./isnackbar-manager.service";
 import { IDialogManagerService } from "./idialog-manager.service";
+import { IScheduleServiceService } from "./api-client/schedules/ischedules.service";
 
 export const SERVICES_TOKEN = {
   HTTP: {
     CLIENT: new InjectionToken<IClientService>("SERVICE_TOKEN.HTTP.CLIENT"),
-    // SCHEDULE: new InjectionToken<IScheduleService>("SERVICE_TOKEN.HTTP.SCHEDULE"),
+    SCHEDULE: new InjectionToken<IScheduleServiceService>("SERVICE_TOKEN.HTTP.SCHEDULE"),
   },
   SNACKBAR: new InjectionToken<ISankBarManagerService>("SERVICE_TOKEN.SNACKBAR"),
   DIALOG: new InjectionToken<IDialogManagerService>("SERVICE_TOKEN.DIALOG")
